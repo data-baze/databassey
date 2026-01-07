9bc17f4545e107836bb2c34b6e20bd69f4586754
data-baze <basseydata@gmail.com>
2026-01-07 10:03:37 +0100

    minor bug fixes

--- src/screens/home/components/SignatureSection.tsx
+++ src/screens/home/components/SignatureSection.tsx
@@ -8,9 +8,9 @@ const SignatureSection = () => {
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        className="flex justify-center gap-4 items-center"
+        className="flex justify-center gap-2 md:gap-4 items-center"
       >
-        <h3 className="text-[30px] md:text-[60px] lg:text-[100px] font-light">
+        <h3 className="text-[20px] md:text-[60px] lg:text-[100px] font-light">
           Data.
         </h3>
 
@@ -20,7 +20,7 @@ const SignatureSection = () => {
           className="w-50 h-50 md:h-[460px] md:w-[460px] object-cover opacity-80 mb-10 mask-[linear-gradient(to_bottom,#0E0E0E_70%,transparent_92%)]"
         />
 
-        <h3 className="text-[30px] md:text-[60px] lg:text-[100px] font-light italic">
+        <h3 className="text-[20px] md:text-[60px] lg:text-[100px] font-light italic">
           Bassey
         </h3>
       </motion.div>
